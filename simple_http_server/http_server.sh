af1e5f25c967e8ad06aa91fcd6033cc8ece141ab
vd141 <victor.dinh141@gmail.com>
2025-09-11 20:27:31 -0400

    web files

--- simple_http_server/http_server.sh
+++ simple_http_server/http_server.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 function server () {
-
+    while true
+    do
+        read method path version
+        if [[ $method == "GET" ]]
+        then
+            echo "HTTP/1.1 200 OK"
+        else
+            echo "HTTP1.1 400 Bad Request"
+        fi
+    done
 }
 
 coproc SERVER_PROCESS { server; }
 
-netcat -lv 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
+nc -lv 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}

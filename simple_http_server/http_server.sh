3f6323e6b8fb5059104abe3b8409a0527f5675eb
Victor Dinh <vd141@users.noreply.github.com>
2025-09-13 18:57:05 +0000

    midday progress 9/13

--- simple_http_server/http_server.sh
+++ simple_http_server/http_server.sh
@@ -3,24 +3,39 @@
 function server () {
     while true
     do
-        read method path version
+        message_arr=()
+        check=true
+        while $check
+        do
+            read line
+            message_arr+=($line)
+            if [[ "${#line}" -eq 0 ]]
+            then
+                check=false
+            fi
+        done
+        method=${message_arr[0]}
+        path=${message_arr[1]}
         if [[ $method == "GET" ]]
         then
-            filepath="./www$path"
-            echo $(pwd)
+            filepath="./www/$path"
             if [[ -f $filepath ]]
             then
-                echo "HTTP/1.1 200 OK\r\n"
-                cat $filepath
+                echo -e "HTTP/1.1 200 OK\r\n"
+                echo "Content-Length: $(wc -c < $filepath)"
+                echo "Content-Type: text/html; charset=utf-8"
+                echo -e "$(cat $filepath)\r\n"
             else
                 echo "HTTP/1.1 404 Not Found"
+                echo "Content-Length: 0"
             fi
         else
             echo "HTTP1.1 400 Bad Request"
+            echo "Content-Length: 0"
         fi
     done
 }
 
 coproc SERVER_PROCESS { server; }
 
-nc -lvp 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
+nc -klvp 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
